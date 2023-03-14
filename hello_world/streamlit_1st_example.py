5d2a04306b59d91d0e596b63bcd2d2dd5de35c9c
stran2468 <stran2468@gmail.com>
2023-03-13 22:38:37 -0400

    1st commit 3/13 10:38pm

---
+++ hello_world/streamlit_1st_example.py
@@ -0,0 +1,11 @@
+# taken from: https://docs.streamlit.io/library/get-started/create-an-app
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+'# Avocado Prices dashboard'
+
+st.title('Avocado Prices dashboard')
+
+DATE_COLUMN = 'date/time'
