587baf59e518bf6896f581aeb829c31e48ec213f
stran2468 <stran2468@gmail.com>
2023-03-13 22:48:11 -0400

    Add headers

--- hello_world/streamlit_1st_example.py
+++ hello_world/streamlit_1st_example.py
@@ -4,6 +4,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-'# Avocado Prices dashboard'
-
-DATE_COLUMN = 'date/time'
+st.write('# Avocado Prices dashboard')  #st.title('Avocado Prices dashboard')
+st.markdown('''
+This is a dashboard showing the *average prices* of different types of :avocado:  
+Data source: [Kaggle](https://www.kaggle.com/datasets/timmate/avocado-prices-2020)
+''')
+st.header('Summary statistics')
+st.header('Line chart by geographies')
